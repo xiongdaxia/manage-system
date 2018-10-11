6236f5207af50e44b3016eb36b006163dd81f8a6
xiongdaxia <792360591@qq.com>
2018-10-11 20:37:13 +0800

    Add files via upload

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,42 @@
+/*
+ * @Author: kangtian@yzyw-inc.com 
+ * @Date: 2018-09-11 10:23:29 
+ * @Last Modified by: kangtian@yzyw-inc.com
+ * @Last Modified time: 2018-09-11 11:21:25
+ */
+<template lang='pug'>
+.h-p100.w-h100
+  el-row
+    el-col(:span="3")
+      .h-60.w-60
+        img.h-p100.w-p100(src="")
+    el-col(:span="12")    
+      span  d
+    el-col(:span="3")
+      span 熊抗天
+    el-col(:span="3")  
+      el-button(type="default" size="small") 账号设置
+    el-col(:span="3")  
+      el-button(type="default" size="small") 退出登录
+
+    
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {};
+  },
+  methods: {
+    // ======================事件处理函数======================
+    // ======================业务逻辑函数======================
+    // ========================纯函数=========================
+    // ======================网络请求函数======================
+    // =======================初始化函数=======================
+  }
+});
+</script>
+
+
---
+++ src/page/Home.vue
@@ -0,0 +1,43 @@
+/*
+ * @Author: kangtian@yzyw-inc.com 
+ * @Date: 2018-09-10 20:04:54 
+ * @Last Modified by: kangtian@yzyw-inc.com
+ * @Last Modified time: 2018-09-11 12:12:57
+ */
+<template lang='pug'>
+div
+  el-container.h-p100
+    el-aside
+      SideBar
+    el-container
+      el-header
+        HeaderBar
+      el-main
+        router-view
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import HeaderBar from '@/components/HeaderBar.vue';
+import SideBar from '@/components/Sidebar.vue';
+
+export default Vue.extend({
+  data() {
+    return {};
+  },
+  components: {
+    HeaderBar,
+    SideBar
+  },
+  methods: {
+    // ======================事件处理函数======================
+    // ======================业务逻辑函数======================
+    // ========================纯函数=========================
+    // ======================网络请求函数======================
+    // =======================初始化函数=======================
+  }
+});
+</script>
+
+<style lang='scss' scoped>
+</style>
